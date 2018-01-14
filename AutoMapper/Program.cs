e5429f3102c0285d85cd264febba2475e9f7ffb3
lutfiqaraman <lutfiqaraman@gmail.com>
2018-01-14 22:04:11 +0200

    Here you go ...

--- AutoMapper/Program.cs
+++ AutoMapper/Program.cs
@@ -10,6 +10,42 @@ namespace AutoMapper
     {
         static void Main(string[] args)
         {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<AuthorDTO, AuthorModel>()
+                .ForMember(
+                    destModel => destModel.Address, 
+                    map => map.MapFrom(SourceModel => new Address
+                    {
+                        City    = SourceModel.City,
+                        State   = SourceModel.State,
+                        Country = SourceModel.Country
+                    }));
+            });
+
+            IMapper iMapper = config.CreateMapper();
+
+            var source = new AuthorDTO()
+            {
+                Id          = 1,
+                FirstName   = "Joydip",
+                LastName    = "Kanjilal",
+                City        = "Amman",
+                State       = "Amman",
+                Country     = "Jordan"
+            };
+
+            var printModel = iMapper.Map<AuthorDTO, AuthorModel>(source);
+
+            Console.WriteLine(
+                "Author Name: " + printModel.FirstName      + " "   + printModel.LastName + "\n" +
+                "City: "        + printModel.Address.City   + "\n"  +
+                "Country: "     + printModel.Address.Country
+                );
+
+
+            Console.ReadLine();
         }
     }
 }
+ 
